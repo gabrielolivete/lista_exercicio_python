cfac467e1b7a5b22c3f0c560d08a8f8d70f3b23c
Gabriel Olivete <115937639+gabrielolivete@users.noreply.github.com>
2024-09-06 17:06:53 -0300

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Programa que mostre a mensagem "Hello World". 
+
+mensagem = "bem vindo ao primeiro grande desafio do mundo da programação."
+print(len(mensagem) * "=")
+print(mensagem.upper())
+print(len(mensagem) * "=", "\n")
+
+print("Hello World!!!")
